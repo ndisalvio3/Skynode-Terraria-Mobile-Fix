a50068f632ff24af32a43297d58abea118bd4349
Nicholas <102550462+ndisalvio3@users.noreply.github.com>
2022-10-03 10:39:54 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ apt install -y curl wget file unzip fastjar
 mkdir -p /mnt/server/
 cd /mnt/server/
 echo -e "Downloading terraria server files"
-DOWNLOAD_LINK=https://github.com/jbndis/Skynode-Terraria-Mobile-Fix/raw/main/ServerLinux.zip
+DOWNLOAD_LINK=https://github.com/ndisalvio3/Skynode-Terraria-Mobile-Fix/blob/main/ServerLinux.zip?raw=true
 ## this is a simple script to validate a download url actaully exists
 CLEAN_VERSION=$(echo ${DOWNLOAD_LINK##*/} | cut -d'-' -f3 | cut -d'.' -f1)
 echo -e "running 'curl -sSL ${DOWNLOAD_LINK} -o ${DOWNLOAD_LINK##*/}'"
-curl -sSL https://github.com/jbndis/Skynode-Terraria-Mobile-Fix/raw/main/ServerLinux.zip -o ${DOWNLOAD_LINK##*/}
+curl -sSL https://github.com/ndisalvio3/Skynode-Terraria-Mobile-Fix/blob/main/ServerLinux.zip?raw=true -o ${DOWNLOAD_LINK##*/}
 echo -e "Unpacking server files"
 jar xvf ${DOWNLOAD_LINK##*/}
 echo -e ""
